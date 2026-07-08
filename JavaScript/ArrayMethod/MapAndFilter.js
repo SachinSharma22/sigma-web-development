a1965a6d60f3133beb47b2ac0614949abb43868c
SachinSharma22 <s830sharma@gmail.com>
2026-07-08 07:32:19 +0530

    chain filter and map both method

--- JavaScript/ArrayMethod/MapAndFilter.js
+++ JavaScript/ArrayMethod/MapAndFilter.js
@@ -19,6 +19,18 @@ let resultGPA = arr.map((el) => {
 
 console.log(resultGPA)
 
+let fruits = ["apple", "banana", "graves"];
+
+let newFruits = fruits.map((el) => {
+    return el.toUpperCase();
+})
+
+let fruitDetail = fruits.map((el, index) => {
+    console.log(index, el);
+})
+
+console.log(newFruits)
+
 // Filter method
 
 let nums = [2,3,4,5,6,7,8,9,0];
@@ -28,3 +40,19 @@ let even = nums.filter((el) =>
 )
 
 console.log(even)
+
+// Filter and Map chain
+const users = [
+    { name: "Sachin", age: 22 },
+    { name: "Rahul", age: 17 },
+    { name: "Amit", age: 30 }
+];
+
+const name = users.filter((el) => {
+    return el.age > 18;
+})
+.map((el) => {
+    return el.name
+})
+
+console.log(name)
