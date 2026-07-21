14e1ce3948b058f35c93f3a85db7800421ae42e9
SachinSharma22 <s830sharma@gmail.com>
2026-07-21 08:57:40 +0530

    promises learning

--- JavaScript/Practice/app.js
+++ JavaScript/Practice/app.js
@@ -1,11 +1,31 @@
-let ptxt = document.getElementById('txt');
-const changeBtn = document.getElementById('btn');
-const fName = document.getElementById('name');
-console.log(fName);
-changeBtn.addEventListener("click", () => {
-    let newPara = document.createElement('p');
-    newPara.innerText = fName.value;
-    document.body.appendChild(newPara);
-    ptxt.innerText = "This is happened with the help of JS";
-})
+// let ptxt = document.getElementById('txt');
+// const changeBtn = document.getElementById('btn');
+// const fName = document.getElementById('name');
+// console.log(fName);
+// changeBtn.addEventListener("click", () => {
+//     let newPara = document.createElement('p');
+//     newPara.innerText = fName.value;
+//     document.body.appendChild(newPara);
+//     ptxt.innerText = "This is happened with the help of JS";
+// })
 
+// Promises
+
+function saveToDb(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 5) {
+      resolve(data);
+    } else {
+      reject("Internet speed is too slow");
+    }
+  });
+}
+
+saveToDb("Sachin Sharma")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
